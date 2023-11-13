5744efe18096b07e97340cb02a9acdaf22db6788
Pedro Klein <pedroklein1@hotmail.com>
2023-11-13 20:52:07 -0300

    🚧 Add new conan packages

--- server/main.cpp
+++ server/main.cpp
@@ -1,17 +1,7 @@
-#include <asio.hpp>
 #include <iostream>
 
-void print(const asio::error_code& /*e*/) {
+int main()
+{
     std::cout << "Hello, world!" << std::endl;
-}
-
-int main() {
-    asio::io_context io;
-
-    asio::steady_timer t(io, asio::chrono::seconds(5));
-    t.async_wait(&print);
-
-    io.run();
-
     return 0;
 }
