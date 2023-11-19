b10a1730fbd3425b733883679d92e154b3dd79e4
Pedro Klein <pedroklein1@hotmail.com>
2023-11-19 23:04:34 +0000

    🚨 Fix pthread lib import

--- server/socket/socket.hpp
+++ server/socket/socket.hpp
@@ -2,50 +2,52 @@
 #define SERVER_SOCKET_HPP
 
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
 #include <errno.h>
 #include <jsoncpp/json/json.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define DEFAULT_PORT 8765
 #define QUEUE_SIZE 5
 #define BUFFER_SIZE 256
 
-typedef struct Connection {
+typedef struct Connection
+{
     int socket;
     pthread_t thread;
 } Connection;
 
 class Socket
 {
-public:
+  public:
     Socket(int port);
     ~Socket();
     void StartListening();
     void StartPingThread();
-private:
-    // Server 
+
+  private:
+    // Server
     int serverSocket;
-    socklen_t clientLength; 
+    socklen_t clientLength;
     struct sockaddr_in serverAddress, clientAddress;
 
-    static void* handleClient(void *arg);
+    static void *handleClient(void *arg);
     static void receiveData(int clientSocket);
 
-    
     // Flag to control the ping thread
     bool pingThreadRunning;
     pthread_t pingThread;
-    static void* pingClients(void* arg);
+    static void *pingClients(void *arg);
 
     struct sockaddr_in newSocketAddress(int port);
 
@@ -57,16 +59,18 @@ Socket::Socket(int port)
 {
     // Socket creation
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1){
+    if (serverSocket == -1)
+    {
         std::cerr << "ERR: failed to create a socket\n\t" << strerror(errno) << std::endl;
         exit(errno);
     }
 
-    // Setting up server address 
+    // Setting up server address
     serverAddress = newSocketAddress(port);
 
-    // Binding create socket 
-    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0){
+    // Binding create socket
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0)
+    {
         std::cerr << "ERR: failed to bind socket\n\t" << strerror(errno) << std::endl;
         exit(errno);
     }
@@ -83,12 +87,15 @@ Socket::~Socket()
     close(serverSocket);
 }
 
-void Socket::StartListening() {
+void Socket::StartListening()
+{
     Connection connection;
-    while (true) {
+    while (true)
+    {
         // Accept a client connection
-        connection.socket = accept(serverSocket, (struct sockaddr *) &clientAddress, &clientLength);
-        if (connection.socket < 0) {
+        connection.socket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientLength);
+        if (connection.socket < 0)
+        {
             std::cerr << "Error on accept." << std::endl;
             exit(EXIT_FAILURE);
         }
@@ -96,7 +103,8 @@ void Socket::StartListening() {
         std::cout << "Client {" << connection.socket << "} connected." << std::endl;
 
         // Create a new thread to handle the client
-        if (pthread_create(&connection.thread, NULL, handleClient, (void *)&connection.socket) != 0) {
+        if (pthread_create(&connection.thread, NULL, handleClient, (void *)&connection.socket) != 0)
+        {
             std::cerr << "Error creating thread." << std::endl;
             exit(EXIT_FAILURE);
         }
@@ -105,9 +113,11 @@ void Socket::StartListening() {
     }
 }
 
-void Socket::StartPingThread() {
+void Socket::StartPingThread()
+{
     pingThreadRunning = true;
-    if (pthread_create(&pingThread, nullptr, &Socket::pingClients, this) != 0) {
+    if (pthread_create(&pingThread, nullptr, &Socket::pingClients, this) != 0)
+    {
         std::cerr << "Error creating ping thread." << std::endl;
         exit(EXIT_FAILURE);
     }
@@ -117,42 +127,46 @@ void Socket::StartPingThread() {
     === Private Funcs ===
 */
 
-void* Socket::handleClient(void* arg) {
-    int clientSocket = *((int *) arg);
+void *Socket::handleClient(void *arg)
+{
+    int clientSocket = *((int *)arg);
     // Maybe a loop for receiving and sending data here,
     // or we should create another thread just for sending data to the clients
     receiveData(clientSocket);
-    
+
     close(clientSocket);
     pthread_exit(NULL);
 }
 
-void Socket::receiveData(int clientSocket) {
+void Socket::receiveData(int clientSocket)
+{
     char buffer[BUFSIZ];
-    while(true) {
+    while (true)
+    {
         bzero(buffer, BUFSIZ);
         ssize_t readBytes = read(clientSocket, buffer, BUFSIZ);
-        if (readBytes > 0){
-            std::cout << "Received Data from client " << clientSocket << "\nData: " << buffer << std::endl; 
-        
+        if (readBytes > 0)
+        {
+            std::cout << "Received Data from client " << clientSocket << "\nData: " << buffer << std::endl;
+
             // Reconstructing Json Object
             std::string receivedJsonStr(buffer, readBytes);
             Json::CharReaderBuilder reader;
             Json::Value jsonObject;
             std::istringstream jsonStringStream(receivedJsonStr);
             Json::parseFromStream(reader, jsonStringStream, &jsonObject, nullptr);
-            std::cout << "Json obj:" << "\n\tuser: " << jsonObject["user"] << "\n\tmsg: " << jsonObject["msg"] << std::endl; 
+            std::cout << "Json obj:"
+                      << "\n\tuser: " << jsonObject["user"] << "\n\tmsg: " << jsonObject["msg"] << std::endl;
         }
-
     }
 }
 
-struct sockaddr_in Socket::newSocketAddress(int port) 
+struct sockaddr_in Socket::newSocketAddress(int port)
 {
-    struct sockaddr_in serverAddress; 
+    struct sockaddr_in serverAddress;
 
     // Set up server address struct
-    bzero((char *) &serverAddress, sizeof(serverAddress));
+    bzero((char *)&serverAddress, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_port = htons(port);
@@ -160,16 +174,19 @@ struct sockaddr_in Socket::newSocketAddress(int port)
     return serverAddress;
 }
 
-void* Socket::pingClients(void* arg) {
-    Socket* socketInstance = static_cast<Socket*>(arg);
+void *Socket::pingClients(void *arg)
+{
+    Socket *socketInstance = static_cast<Socket *>(arg);
 
-    Json::Value data; 
+    Json::Value data;
     data["toUsr"] = "";
     data["msg"] = "ping";
 
-    while (socketInstance->pingThreadRunning) {
+    while (socketInstance->pingThreadRunning)
+    {
         // Send ping message to each connected client
-        for (auto& connection : socketInstance->clientConnections) {
+        for (auto &connection : socketInstance->clientConnections)
+        {
             data["toUsr"] = connection.socket;
             std::ostringstream jsonStringStream;
             jsonStringStream << data;
@@ -177,7 +194,8 @@ void* Socket::pingClients(void* arg) {
             std::string pingMessage = jsonStringStream.str();
 
             ssize_t sentBytes = send(connection.socket, pingMessage.c_str(), pingMessage.length(), 0);
-            if (sentBytes == -1) {
+            if (sentBytes == -1)
+            {
                 // Handle error if needed
                 std::cerr << "Error sending ping to client " << connection.socket << std::endl;
             }
@@ -189,6 +207,5 @@ void* Socket::pingClients(void* arg) {
 
     pthread_exit(nullptr);
 }
-
 
 #endif
