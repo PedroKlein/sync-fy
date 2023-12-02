457334e390afa0b4798817883e30ee0b0e01d3df
henriqueutzig <henriqueutzig@gmail.com>
2023-12-02 10:55:44 -0300

    📝 Add doxygen to client file and server monitor

--- client/includes/localMonitor/localMonitor.hpp
+++ client/includes/localMonitor/localMonitor.hpp
@@ -10,13 +10,34 @@
 namespace localMonitor
 {
 
+/**
+ * @class LocalMonitor
+ * @brief Class that monitors a directory for file changes and handles corresponding events.
+ */
 class LocalMonitor
 {
   public:
+    /**
+     * @brief Constructor for LocalMonitor.
+     * @param fileWatcher The FileWatcher responsible for monitoring file changes.
+     * @param messageHandler The ClientMessageHandler for handling messages related to file changes.
+     */
     LocalMonitor(FileWatcher &fileWatcher, const ClientMessageHandler &messageHandler);
+
+    /**
+     * @brief Destructor for LocalMonitor.
+     */
     ~LocalMonitor();
 
+    /**
+     * @brief Starts the local monitoring in a separate thread.
+     * @return A pointer to the thread handling the local monitoring.
+     */
     std::thread *start();
+
+    /**
+     * @brief Stops the local monitoring and joins the monitoring thread.
+     */
     void stop();
 
   private:
@@ -25,8 +46,21 @@ class LocalMonitor
     FileWatcher &fileWatcher;
     const ClientMessageHandler &messageHandler;
 
+    /**
+     * @brief Internal function to run the local monitoring in a separate thread.
+     */
     void run();
+
+    /**
+     * @brief Callback function for handling file added or modified events.
+     * @param filePath The path of the added or modified file.
+     */
     void onFileAddedOrModified(const std::string &filePath);
+
+    /**
+     * @brief Callback function for handling file removed events.
+     * @param filePath The path of the removed file.
+     */
     void onFileRemoved(const std::string &filePath);
 };
 } // namespace localMonitor
