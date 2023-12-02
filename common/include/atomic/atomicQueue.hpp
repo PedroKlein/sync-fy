9d2efd083a1931adf036a65435de7b0d4d507354
henriqueutzig <henriqueutzig@gmail.com>
2023-12-02 11:27:44 -0300

    📝 Add doxygen documentation to server

--- common/include/atomic/atomicQueue.hpp
+++ common/include/atomic/atomicQueue.hpp
@@ -4,6 +4,10 @@
 
 namespace common
 {
+/**
+ * @brief Template class representing a thread-safe queue with atomic operations.
+ * @tparam T The type of elements stored in the queue.
+ */
 template <typename T> class AtomicQueue
 {
   private:
@@ -11,18 +15,35 @@ template <typename T> class AtomicQueue
     std::mutex mtx;
 
   public:
+    /**
+     * @brief Default constructor for AtomicQueue.
+     */
     AtomicQueue() = default;
+
+    /**
+     * @brief Default destructor for AtomicQueue.
+     */
     ~AtomicQueue() = default;
 
+    // Copy operations are deleted to prevent unintended behavior in a multi-threaded environment.
     AtomicQueue(const AtomicQueue &) = delete;
     AtomicQueue &operator=(const AtomicQueue &) = delete;
 
+    /**
+     * @brief Pushes an element onto the queue in a thread-safe manner.
+     * @param data The element to be pushed onto the queue.
+     */
     void push(const T &data)
     {
         std::lock_guard<std::mutex> lock(mtx);
         queue.push(data);
     }
 
+    /**
+     * @brief Pops an element from the queue in a thread-safe manner.
+     * @return The element popped from the queue.
+     * @throws std::out_of_range if the queue is empty.
+     */
     T pop()
     {
         std::lock_guard<std::mutex> lock(mtx);
@@ -35,6 +56,11 @@ template <typename T> class AtomicQueue
         return data;
     }
 
+    /**
+     * @brief Attempts to pop an element from the queue in a thread-safe manner.
+     * @param data A reference to store the popped element.
+     * @return True if an element was successfully popped, false if the queue is empty.
+     */
     bool tryPop(T &data)
     {
         std::lock_guard<std::mutex> lock(mtx);
@@ -47,6 +73,10 @@ template <typename T> class AtomicQueue
         return true;
     }
 
+    /**
+     * @brief Checks if the queue is empty in a thread-safe manner.
+     * @return True if the queue is empty, false otherwise.
+     */
     bool empty()
     {
         std::lock_guard<std::mutex> lock(mtx);
--- server/includes/serverSocket.hpp
+++ server/includes/serverSocket.hpp
@@ -9,18 +9,45 @@
 
 using OnConnectionCallback = std::function<void(int, std::string)>;
 
+/**
+ * @class ServerSocket
+ * @brief Class representing a TCP server socket.
+ */
 class ServerSocket : public common::TCPSocket
 {
   public:
+    /**
+     * @brief Constructor for ServerSocket.
+     * @param port The port on which the server socket will listen for incoming connections.
+     * @param onClientConnectCallback The callback function to be executed when a client connects.
+     */
     ServerSocket(int port, OnConnectionCallback onClientConnectCallback);
+
+    /**
+     * @brief Default destructor for ServerSocket.
+     */
     ~ServerSocket();
+
+    /**
+     * @brief Starts listening for incoming connections.
+     */
     void startListening();
+
+    /**
+     * @brief Stops listening for incoming connections.
+     */
     void stopListening();
 
   private:
     bool isListening;
     socklen_t clientLength;
     struct sockaddr_in serverAddress, clientAddress;
+
+    /**
+     * @brief Creates a new sockaddr_in structure with the specified port.
+     * @param port The port for the new sockaddr_in structure.
+     * @return The newly created sockaddr_in structure.
+     */
     struct sockaddr_in newSocketAddress(int port);
 
     OnConnectionCallback onClientConnectCallback;
