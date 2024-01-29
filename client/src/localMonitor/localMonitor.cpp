6b5c1a51300bd999f7d809cb0456dbfbf2daa775
Pedro Klein <pedroklein1@hotmail.com>
2024-01-29 16:51:19 +0000

    🚑 Fix fix fix

--- client/src/localMonitor/localMonitor.cpp
+++ client/src/localMonitor/localMonitor.cpp
@@ -34,7 +34,7 @@ void LocalMonitor::run()
     do
     {
         fileWatcher.processEvents();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     } while (isRunning);
 }
 
